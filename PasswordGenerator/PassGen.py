760293c3cea48bd84943bd934b1bca79cf8a78eb
DennisLent <62008839+DennisLent@users.noreply.github.com>
2022-04-19 22:41:59 +0200

    added PasswordGenerator
    Started working on a password generator

---
+++ PasswordGenerator/PassGen.py
@@ -0,0 +1,12 @@
+import random
+
+def PassGen(length, *, capital=True, numbers=False, special_symbols=False):
+    """
+
+    :param length: length of password
+    :param capital: boolean -> use capital letters
+    :param numbers:
+    :param special_symbols:
+    :return:
+    """
+    pass
